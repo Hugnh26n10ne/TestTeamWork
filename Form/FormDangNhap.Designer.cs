9d08f033835c997042a9ed26524c30d06da6d4f9
Hugnh26n10ne <hugnh26n10ne@gmail.com>
2023-12-19 16:28:36 +0700

    c3

---
+++ Form/FormDangNhap.Designer.cs
@@ -0,0 +1,309 @@
+﻿namespace PhanMemQuanLyDiemSinhVien
+{
+    partial class FormDangNhap
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormDangNhap));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label5 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.pbx_Hide = new System.Windows.Forms.PictureBox();
+            this.pbx_Show = new System.Windows.Forms.PictureBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.pbx_Help = new System.Windows.Forms.PictureBox();
+            this.lb_QuenMK = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cbx_GhiNho = new System.Windows.Forms.CheckBox();
+            this.lb_DangKi = new System.Windows.Forms.Label();
+            this.btn_DangNhap = new System.Windows.Forms.Button();
+            this.tbx_MatKhau = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbx_TaiKhoan = new System.Windows.Forms.TextBox();
+            this.lb_Tk = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx_Hide)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx_Show)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx_Help)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Transparent;
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.groupBox1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(3, 6, 3, 6);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(956, 616);
+            this.panel1.TabIndex = 1;
+            // 
+            // label5
+            // 
+            this.label5.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label5.Font = new System.Drawing.Font("UTM Alexander", 31.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.label5.Location = new System.Drawing.Point(265, 9);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(396, 104);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "ĐĂNG NHẬP";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.groupBox1.BackColor = System.Drawing.Color.White;
+            this.groupBox1.Controls.Add(this.pbx_Hide);
+            this.groupBox1.Controls.Add(this.pbx_Show);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.pbx_Help);
+            this.groupBox1.Controls.Add(this.lb_QuenMK);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.cbx_GhiNho);
+            this.groupBox1.Controls.Add(this.lb_DangKi);
+            this.groupBox1.Controls.Add(this.btn_DangNhap);
+            this.groupBox1.Controls.Add(this.tbx_MatKhau);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.tbx_TaiKhoan);
+            this.groupBox1.Controls.Add(this.lb_Tk);
+            this.groupBox1.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(290, 127);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(3, 6, 3, 6);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(3, 6, 3, 6);
+            this.groupBox1.Size = new System.Drawing.Size(330, 453);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Thông tin đăng nhập";
+            // 
+            // pbx_Hide
+            // 
+            this.pbx_Hide.BackColor = System.Drawing.Color.Transparent;
+            this.pbx_Hide.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pbx_Hide.Image = global::PhanMemQuanLyDiemSinhVien.Properties.Resources.hide;
+            this.pbx_Hide.Location = new System.Drawing.Point(261, 165);
+            this.pbx_Hide.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.pbx_Hide.Name = "pbx_Hide";
+            this.pbx_Hide.Size = new System.Drawing.Size(35, 20);
+            this.pbx_Hide.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbx_Hide.TabIndex = 15;
+            this.pbx_Hide.TabStop = false;
+            this.pbx_Hide.Click += new System.EventHandler(this.pbx_Hide_Click);
+            // 
+            // pbx_Show
+            // 
+            this.pbx_Show.BackColor = System.Drawing.Color.Transparent;
+            this.pbx_Show.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.pbx_Show.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pbx_Show.Image = global::PhanMemQuanLyDiemSinhVien.Properties.Resources.show;
+            this.pbx_Show.Location = new System.Drawing.Point(261, 165);
+            this.pbx_Show.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.pbx_Show.Name = "pbx_Show";
+            this.pbx_Show.Size = new System.Drawing.Size(35, 20);
+            this.pbx_Show.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbx_Show.TabIndex = 16;
+            this.pbx_Show.TabStop = false;
+            this.pbx_Show.Click += new System.EventHandler(this.pbx_Show_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.label4.Location = new System.Drawing.Point(30, 420);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(59, 23);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Hỗ trợ";
+            // 
+            // pbx_Help
+            // 
+            this.pbx_Help.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pbx_Help.Image = global::PhanMemQuanLyDiemSinhVien.Properties.Resources.help;
+            this.pbx_Help.Location = new System.Drawing.Point(107, 414);
+            this.pbx_Help.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.pbx_Help.Name = "pbx_Help";
+            this.pbx_Help.Size = new System.Drawing.Size(45, 34);
+            this.pbx_Help.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbx_Help.TabIndex = 12;
+            this.pbx_Help.TabStop = false;
+            this.pbx_Help.Click += new System.EventHandler(this.pbx_Help_Click);
+            // 
+            // lb_QuenMK
+            // 
+            this.lb_QuenMK.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.lb_QuenMK.AutoSize = true;
+            this.lb_QuenMK.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.lb_QuenMK.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb_QuenMK.ForeColor = System.Drawing.Color.Green;
+            this.lb_QuenMK.Location = new System.Drawing.Point(161, 224);
+            this.lb_QuenMK.Name = "lb_QuenMK";
+            this.lb_QuenMK.Size = new System.Drawing.Size(137, 23);
+            this.lb_QuenMK.TabIndex = 14;
+            this.lb_QuenMK.Text = "Quên mật khẩu?";
+            this.lb_QuenMK.Click += new System.EventHandler(this.lb_QuenMK_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(30, 383);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(188, 23);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Bạn chưa có tài khoản?";
+            // 
+            // cbx_GhiNho
+            // 
+            this.cbx_GhiNho.AutoSize = true;
+            this.cbx_GhiNho.Location = new System.Drawing.Point(31, 220);
+            this.cbx_GhiNho.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.cbx_GhiNho.Name = "cbx_GhiNho";
+            this.cbx_GhiNho.Size = new System.Drawing.Size(93, 27);
+            this.cbx_GhiNho.TabIndex = 13;
+            this.cbx_GhiNho.Text = "Ghi nhớ";
+            this.cbx_GhiNho.UseVisualStyleBackColor = true;
+            // 
+            // lb_DangKi
+            // 
+            this.lb_DangKi.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.lb_DangKi.AutoSize = true;
+            this.lb_DangKi.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.lb_DangKi.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb_DangKi.ForeColor = System.Drawing.Color.Red;
+            this.lb_DangKi.Location = new System.Drawing.Point(228, 383);
+            this.lb_DangKi.Name = "lb_DangKi";
+            this.lb_DangKi.Size = new System.Drawing.Size(70, 23);
+            this.lb_DangKi.TabIndex = 8;
+            this.lb_DangKi.Text = "Đăng Kí";
+            this.lb_DangKi.Click += new System.EventHandler(this.lb_DangKi_Click);
+            // 
+            // btn_DangNhap
+            // 
+            this.btn_DangNhap.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.btn_DangNhap.BackColor = System.Drawing.Color.Tomato;
+            this.btn_DangNhap.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_DangNhap.FlatAppearance.BorderSize = 0;
+            this.btn_DangNhap.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_DangNhap.Font = new System.Drawing.Font("UTM Banque", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_DangNhap.ForeColor = System.Drawing.Color.White;
+            this.btn_DangNhap.Location = new System.Drawing.Point(31, 280);
+            this.btn_DangNhap.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btn_DangNhap.Name = "btn_DangNhap";
+            this.btn_DangNhap.Size = new System.Drawing.Size(267, 62);
+            this.btn_DangNhap.TabIndex = 4;
+            this.btn_DangNhap.Text = "Đăng Nhập";
+            this.btn_DangNhap.UseVisualStyleBackColor = false;
+            this.btn_DangNhap.Click += new System.EventHandler(this.btn_DangNhap_Click);
+            // 
+            // tbx_MatKhau
+            // 
+            this.tbx_MatKhau.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbx_MatKhau.Location = new System.Drawing.Point(31, 164);
+            this.tbx_MatKhau.Margin = new System.Windows.Forms.Padding(3, 6, 3, 6);
+            this.tbx_MatKhau.Name = "tbx_MatKhau";
+            this.tbx_MatKhau.Size = new System.Drawing.Size(267, 30);
+            this.tbx_MatKhau.TabIndex = 3;
+            this.tbx_MatKhau.UseSystemPasswordChar = true;
+            this.tbx_MatKhau.MouseClick += new System.Windows.Forms.MouseEventHandler(this.tbx_MatKhau_MouseClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(31, 135);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 23);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Mật khẩu";
+            // 
+            // tbx_TaiKhoan
+            // 
+            this.tbx_TaiKhoan.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbx_TaiKhoan.Location = new System.Drawing.Point(31, 84);
+            this.tbx_TaiKhoan.Margin = new System.Windows.Forms.Padding(3, 6, 3, 6);
+            this.tbx_TaiKhoan.Name = "tbx_TaiKhoan";
+            this.tbx_TaiKhoan.Size = new System.Drawing.Size(267, 30);
+            this.tbx_TaiKhoan.TabIndex = 1;
+            this.tbx_TaiKhoan.MouseClick += new System.Windows.Forms.MouseEventHandler(this.tbx_TaiKhoan_MouseClick);
+            // 
+            // lb_Tk
+            // 
+            this.lb_Tk.AutoSize = true;
+            this.lb_Tk.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb_Tk.Location = new System.Drawing.Point(31, 55);
+            this.lb_Tk.Name = "lb_Tk";
+            this.lb_Tk.Size = new System.Drawing.Size(82, 23);
+            this.lb_Tk.TabIndex = 0;
+            this.lb_Tk.Text = "Tài khoản";
+            // 
+            // FormDangNhap
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 23F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(956, 616);
+            this.Controls.Add(this.panel1);
+            this.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.MinimumSize = new System.Drawing.Size(974, 663);
+            this.Name = "FormDangNhap";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Phần mềm quản lý điểm sinh viên";
+            this.Load += new System.EventHandler(this.FormDangNhap_Load);
+            this.panel1.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx_Hide)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx_Show)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbx_Help)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.PictureBox pbx_Help;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lb_DangKi;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btn_DangNhap;
+        private System.Windows.Forms.TextBox tbx_MatKhau;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbx_TaiKhoan;
+        private System.Windows.Forms.Label lb_Tk;
+        private System.Windows.Forms.Label lb_QuenMK;
+        private System.Windows.Forms.CheckBox cbx_GhiNho;
+        private System.Windows.Forms.PictureBox pbx_Hide;
+        private System.Windows.Forms.PictureBox pbx_Show;
+    }
+}
---
+++ Form/FormDangNhap.cs
@@ -0,0 +1,233 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PhanMemQuanLyDiemSinhVien
+{
+    public partial class FormDangNhap : Form
+    {
+        public SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["conn"].ConnectionString);
+        public FormDangNhap()
+        {
+            InitializeComponent();
+        }
+        public void AuthoLogin()
+        {
+            try
+            {
+
+                if (conn.State == ConnectionState.Closed)
+                {
+                    conn.Open();
+                }
+
+                SqlCommand cmd = new SqlCommand();
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.CommandText = "SP_AuthoLogin";
+                cmd.Parameters.AddWithValue("@tai_khoan", tbx_TaiKhoan.Text);
+                cmd.Parameters.AddWithValue("@mat_khau", tbx_MatKhau.Text);
+                cmd.Connection = conn;
+
+                object kq = cmd.ExecuteScalar();
+                int ketqua = Convert.ToInt32(kq);
+
+                if (ketqua == 0)
+                {
+                    MessageBox.Show($"Chào mừng User: {tbx_TaiKhoan.Text} đăng nhập", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                }
+                else if (ketqua == 1)
+                {
+                    MessageBox.Show($"Chào mừng Giáo viên: {tbx_TaiKhoan.Text} đăng nhập", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                }
+                else if (ketqua == 2)
+                {
+                    MessageBox.Show("Chào mừng Admin đăng nhập", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                }
+                else if (ketqua == 3)
+                {
+                    MessageBox.Show("Tài khoản hoặc mật khẩu không đúng !!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    tbx_TaiKhoan.Text = "";
+                    tbx_MatKhau.Text = "";
+                    tbx_TaiKhoan.Focus();
+                    return;
+                }
+                else
+                {
+                    MessageBox.Show("Tài khoản không tồn tại !!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    tbx_TaiKhoan.Text = "";
+                    tbx_MatKhau.Text = "";
+                    tbx_TaiKhoan.Focus();
+                    return;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+
+        }
+        
+        public int GetLevel(string tai_khoan)
+        {
+            int ketQua = 0;
+
+            try
+            {
+                if (conn.State == ConnectionState.Closed)
+                {
+                    conn.Open();
+                }
+
+                string query = "SELECT idPer FROM [73DCTT23_QLDiemSV].[dbo].[User] WHERE [tai_khoan] = @tai_khoan";
+
+                using (SqlCommand cmd = new SqlCommand(query, conn))
+                {
+                    cmd.Parameters.AddWithValue("@tai_khoan", tai_khoan);
+
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        if (reader.Read()) // Di chuyển con trỏ đến hàng đầu tiên
+                        {
+                            // Đọc giá trị từ cột "idPer"
+                            ketQua = reader.GetInt32(reader.GetOrdinal("idPer"));
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                // Xử lý ngoại lệ nếu có
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                // Đảm bảo rằng kết nối được đóng dù có lỗi hay không
+                if (conn.State == ConnectionState.Open)
+                {
+                    conn.Close();
+                }
+            }
+
+            return ketQua;
+        }
+
+        private void ReadSettings()
+        {
+            if (Properties.Settings.Default.remember == true)
+            {
+                tbx_TaiKhoan.Text = Properties.Settings.Default.tai_khoan;
+                tbx_MatKhau.Text = Properties.Settings.Default.mat_khau;
+                cbx_GhiNho.Checked = true;
+            }
+            else
+            {
+                tbx_TaiKhoan.Text = "";
+                tbx_MatKhau.Text = "";
+                cbx_GhiNho.Checked = false;
+            }
+
+        }
+        private void SaveSettings(int level)
+        {
+            if (cbx_GhiNho.Checked)
+            {
+                Properties.Settings.Default.tai_khoan = tbx_TaiKhoan.Text;
+                Properties.Settings.Default.mat_khau = tbx_MatKhau.Text;
+                Properties.Settings.Default.remember = true;
+                Properties.Settings.Default.level = level;
+                Properties.Settings.Default.Save();
+            }
+            else
+            {
+                Properties.Settings.Default.tai_khoan = tbx_TaiKhoan.Text;
+                Properties.Settings.Default.mat_khau = "";
+                Properties.Settings.Default.remember = false;
+                Properties.Settings.Default.level = level;
+                Properties.Settings.Default.Save();
+            }
+
+        }
+
+
+        private void btn_DangNhap_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(tbx_TaiKhoan.Text))
+            {
+                MessageBox.Show("Không được để trống!");
+                tbx_TaiKhoan.Focus();
+            }
+            if (string.IsNullOrEmpty(tbx_MatKhau.Text))
+            {
+                MessageBox.Show("Không được để trống!");
+                tbx_MatKhau.Focus();
+            }
+            AuthoLogin();
+
+            SaveSettings(GetLevel(tbx_TaiKhoan.Text));
+
+        }
+
+        private void pbx_Show_Click(object sender, EventArgs e)
+        {
+            pbx_Show.Hide();
+            pbx_Hide.Show();
+            tbx_MatKhau.UseSystemPasswordChar = true;
+        }
+
+        private void pbx_Hide_Click(object sender, EventArgs e)
+        {
+            pbx_Hide.Hide();
+            pbx_Show.Show();
+            tbx_MatKhau.UseSystemPasswordChar = false;
+        }
+
+        private void FormDangNhap_Load(object sender, EventArgs e)
+        {
+            ReadSettings();
+        }
+
+        private void pbx_Help_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hãy gọi theo số 09xxxxxx90 hoặc nhắn tin zalo theo số 09xxxxxx90 để được hỗ trợ!");
+        }
+
+        private void tbx_TaiKhoan_MouseClick(object sender, MouseEventArgs e)
+        {
+            tbx_TaiKhoan.SelectAll();
+        }
+
+        private void tbx_MatKhau_MouseClick(object sender, MouseEventArgs e)
+        {
+            tbx_MatKhau.SelectAll();
+        }
+
+        private void lb_DangKi_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            FormDangKi fdk = new FormDangKi();
+            fdk.Show();
+        }
+
+        private void lb_QuenMK_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            FormQuenMK fqmk = new FormQuenMK();
+            fqmk.Show();
+        }
+    }
+}
